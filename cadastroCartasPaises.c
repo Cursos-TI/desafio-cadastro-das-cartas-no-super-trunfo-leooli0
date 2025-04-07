f20d8a03d1c0e0a5a12791c5edf881b22827b2b1
leooli0 <leonardosantos300305@gmail.com>
2025-04-07 19:01:11 -0300

    Realizando a introducao e calculos da Densidade Populacional e PIB Percapita

--- cadastroCartasPaises.c
+++ cadastroCartasPaises.c
@@ -13,13 +13,15 @@ int main()
         - Numero de Pontos Turisticos
         - Area (em km²)
         - PIB (Produto Interno Bruto)
+        - Densidade Populacional
+        - PIB PER Capita
 
     */
 
     // Declarando as variaveis para armazenar os dados das cidades
     char estados[50], nomeCidades[50], codigoCidades[10];
     int populacao, numeroPontosTuristicos;
-    float area, pib;
+    float area, pib, pibPerCapita, densidadePopulacional;
 
     // Fazendo a coleta dos dados da primeira cidade
     printf("=== Cadastro da Primeira Cidade ===\n");
@@ -45,14 +47,21 @@ int main()
     printf("Digite o PIB: \n");
     scanf("%f", &pib);
 
+    // realizando os calculos de PIB Per Capita e Densidade Populacional da primeira cidade
+    densidadePopulacional = populacao / area;
+    pibPerCapita = pib / populacao;
+
     // Exibição dos dados da primeira cidade cadastrada
     printf("\n--- Dados da Primeira Cidade ---\n");
     printf("Nome do Estado: %s\n", estados);
     printf("Nome da Cidade: %s\n", nomeCidades);
     printf("Populacao: %d\n", populacao);
     printf("Numero de Pontos Turisticos: %d\n", numeroPontosTuristicos);
-    printf("Area: %.2f km²\n", area, pib);
+    printf("Area: %.2f km²\n", area);
     printf("PIB: %.2f bilhoes de reais\n", pib);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional);
+    printf("PIB Per Capita: %.2f bilhoes de reais\n", pibPerCapita);
+
 
     // Cadastrando a exibicao dos dados da segunda cidade
     printf("\n=== Cadastro da Segunda Cidade ===\n");
@@ -78,6 +87,10 @@ int main()
     printf("Digite o PIB: \n");
     scanf("%f", &pib);
 
+    // realizando os calculos de PIB Per Capita e Densidade Populacional da segunda cidade
+    densidadePopulacional = populacao / area;
+    pibPerCapita = pib / populacao;
+
     // Exibindo os dados da segunda cidade cadastrada
     printf("\n--- Dados da Segunda Cidade ---\n");
     printf("Nome do Estado: %s\n", estados);
@@ -86,6 +99,8 @@ int main()
     printf("Numero de Pontos Turisticos: %d\n", numeroPontosTuristicos);
     printf("Area: %.2f km²\n", area);
     printf("PIB: %.2f  bilhoes de reais\n", pib);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional);
+    printf("PIB Per Capita: %.2f bilhoes de reais\n", pibPerCapita);
 
-    return 0; // finalizando o programa co um retorno padrao
+    return 0; // finalizando o programa com um retorno padrao
 }
